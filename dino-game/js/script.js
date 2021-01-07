27e6e8679de66b6264fdc1b64b37ef4da3484918
Victor Bruneli <vic.bruneli@hotmail.com>
2021-01-07 10:51:14 -0300

    Criando funções e estilos Dino/Cactus

--- dino-game/js/script.js
+++ dino-game/js/script.js
@@ -1,24 +1,31 @@
 console.log("Início - Jogo do Dinossauro!");
 
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
+let isJumping = false;
+let position = 0;
 
 //keycode.info verificar qual numeracao da tecla
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
-    jump();
+    if (!isJumping) {
+      jump();
+    }
   }
 }
 
 function jump() {
-  let position = 0;
+  isJumping = true;
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
       clearInterval(upInterval);
+
       //descendo
       let downInterval = setInterval(() => {
         if (position <= 0) {
           clearInterval(downInterval);
+          isJumping = false;
         } else {
           position -= 20;
           dino.style.bottom = position + "px";
@@ -32,4 +39,33 @@ function jump() {
   }, 20); //executado a cada 20 mili secunds
 }
 
+function creatCactus() {
+  const cactus = document.createElement("div");
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  console.log(randomTime);
+
+  cactus.classList.add("cactus");
+  cactus.style.left = 1000 + "px";
+  background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // game over
+
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo !</h1>';
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + "px";
+    }
+  }, 20);
+  setTimeout(creatCactus, randomTime);
+}
+
+creatCactus();
 document.addEventListener("keyup", handleKeyUp);
