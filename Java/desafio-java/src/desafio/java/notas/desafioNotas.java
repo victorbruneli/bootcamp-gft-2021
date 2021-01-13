c6f5228e53529387c61335d9460aa3d70b86fd4a
Victor Bruneli <vic.bruneli@hotmail.com>
2021-01-13 13:28:39 -0300

    Projeto Java

---
+++ Java/desafio-java/src/desafio/java/notas/desafioNotas.java
@@ -0,0 +1,26 @@
+package desafio.java.notas;
+
+import java.util.Scanner;
+
+
+public class desafioNotas {
+    public static void main(String[] args) {
+         Scanner leitor = new Scanner(System.in);
+
+        int nota = leitor.nextInt();
+
+        if (nota == 0) {
+            System.out.println('E');
+        } else if (nota >= 1 && nota <= 35) {
+            System.out.println('D');
+        } else if (nota >= 36 && nota <= 60) {
+            System.out.println('C');
+        } else if (nota >= 61 && nota <= 85) {
+            System.out.println('B');
+        } else if (nota >= 86 && nota <= 100) {
+            System.out.println('A');
+        }
+    
+        leitor.close();
+    }
+}
---
+++ Java/desafio-java/src/desafio/java/numeros/desafioNumeros.java
@@ -0,0 +1,17 @@
+package desafio.java.numeros;
+
+import java.util.Scanner;
+
+public class desafioNumeros {
+     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int a = Integer.parseInt(leitor.next());
+
+        for (int i = 0; i <= a; i++) {
+            if (i % 2 == 0 && i != 0) {
+                System.out.println(i);
+            }
+        }
+        leitor.close();
+    }
+}
